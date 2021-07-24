db1b5855b8afb47fd81e544b1f6b87d6ee13be3c
togana <o.togana@gmail.com>
2021-07-25 03:09:55 +0900

    Update fizzbuzz.rb
    ruby の === はゆるい比較になるので == に変更した
    
    https://docs.ruby-lang.org/ja/latest/method/Module/i/=3d=3d=3d.html

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,9 @@
 1.upto(20) { |i|
-  if i % 5 === 0 && i % 3 === 0
+  if i % 5 == 0 && i % 3 == 0
     puts 'FizzBuzz'
-  elsif i % 5 === 0
+  elsif i % 5 == 0
     puts 'Buzz'
-  elsif i % 3 === 0
+  elsif i % 3 == 0
     puts 'Fizz'
   else
     puts i
